34c9608e745ed2bc5b06649d0770620221fc668a
Stephen Roberts <stoiver@gmail.com>
2024-02-13 19:51:02 +1030

    need conf.py to upload docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,9 +16,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ANUGA Viewer'
+project = 'ANUGA viewer'
 copyright = 'Commonwealth of Australia (Geoscience Australia) and the Australian National University 2004-Now'
-author =  'Stephen Roberts, Ole Nielsen'
+author =  'Stephen Roberts, Ole Nielsen, Gareth Davies'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
@@ -41,6 +41,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'nbsphinx',
 ]
 
 def linkcode_resolve(domain, info):
@@ -52,6 +53,7 @@ def linkcode_resolve(domain, info):
     return "https://somesite/sourcerepo/%s.py" % filename
 
 
+#autodoc_mock_imports = ["anuga"]
 
 autodoc_default_flags = ['members']
 autosummary_generate = True
